3f0d04371c5563ba5b26faf1374b20e3768ff195
Pascal Hurni <phi@pragmaticpurist.org>
2023-01-21 23:42:16 +0100

    Remove unused dashboard view

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GameController::class, 'promotion'])->middleware(['auth', 'verified'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
