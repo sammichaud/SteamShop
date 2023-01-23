1f037a5f6008955346ffe4503503af29cf4fb913
Samuel Michaud <samuel.michaud2000@gmail.com>
2023-01-23 10:55:27 +0100

    add an owner to a game

---
+++ database/migrations/2023_01_23_094427_add_owner_in_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->unsignedBigInteger('owner_id');
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,82 +29,89 @@ class DatabaseSeeder extends Seeder
         $image_name = 'pacman.png';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Pac Man",
-            'price' => 9.95,
-            'description' => "Escape from the maze",
-            'image_path' => $path,
-            'release_date' => now()->subYears(30)
+            'name'         => "Pac Man",
+            'price'        => 9.95,
+            'description'  => "Escape from the maze",
+            'image_path'   => $path,
+            'release_date' => now()->subYears(30),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'Antiriad.jpg';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Antiriad",
-            'price' => 14.99,
-            'description' => "The sacred armor",
-            'image_path' => $path,
-            'release_date' => now()->subDays(10)
+            'name'         => "Antiriad",
+            'price'        => 14.99,
+            'description'  => "The sacred armor",
+            'image_path'   => $path,
+            'release_date' => now()->subDays(10),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'greenberet.jpg';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Green Beret",
-            'price' => 17.20,
-            'description' => "Save the world",
-            'image_path' => $path,
-            'release_date' => now()->subMonths(2)
+            'name'         => "Green Beret",
+            'price'        => 17.20,
+            'description'  => "Save the world",
+            'image_path'   => $path,
+            'release_date' => now()->subMonths(2),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'ghostngoblins.gif';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Ghost'n'Goblins",
-            'price' => 17.20,
-            'description' => "Free the princess",
-            'image_path' => $path,
-            'release_date' => now()->subMonths(1)
+            'name'         => "Ghost'n'Goblins",
+            'price'        => 17.20,
+            'description'  => "Free the princess",
+            'image_path'   => $path,
+            'release_date' => now()->subMonths(1),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'fzero.jpg';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "F-Zero",
-            'price' => 20.00,
-            'description' => "Drive fast",
-            'image_path' => $path,
-            'release_date' => now()->addMinutes(10)
+            'name'         => "F-Zero",
+            'price'        => 20.00,
+            'description'  => "Drive fast",
+            'image_path'   => $path,
+            'release_date' => now()->addMinutes(10),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'BloodMoney.png';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Blood Money",
-            'price' => 35.00,
-            'description' => "Scroll to the end",
-            'image_path' => $path,
-            'release_date' => now()->subMonth(18)
+            'name'         => "Blood Money",
+            'price'        => 35.00,
+            'description'  => "Scroll to the end",
+            'image_path'   => $path,
+            'release_date' => now()->subMonth(18),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'Shadow-of-the-Beast.jpg';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Shadow of the Beast",
-            'price' => 29.00,
-            'description' => "Found it?",
-            'image_path' => $path,
-            'release_date' => now()->addDays(30)
+            'name'         => "Shadow of the Beast",
+            'price'        => 29.00,
+            'description'  => "Found it?",
+            'image_path'   => $path,
+            'release_date' => now()->addDays(30),
+            'owner_id'     => $user->id,
         ]);
 
         $image_name = 'DungeonMaster.jpg';
         copy(__DIR__.'/'.$image_name, public_path($path = 'images/games/'.$image_name));
         \App\Models\Game::create([
-            'name' => "Dungeon Master",
-            'price' => 39.90,
-            'description' => "Zo Kath Ra",
-            'image_path' => $path,
-            'release_date' => now()->addDays(1)
+            'name'         => "Dungeon Master",
+            'price'        => 39.90,
+            'description'  => "Zo Kath Ra",
+            'image_path'   => $path,
+            'release_date' => now()->addDays(1),
+            'owner_id'     => $user->id,
         ]);
-
     }
 }
